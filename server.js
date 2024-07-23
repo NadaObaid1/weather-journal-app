01516fd26485cf6f7f6bfd7dc6767efa93160ffd
Nada Obaid <nada.s.obaidd@gmail.com>
2024-07-23 15:49:33 +0300

    add some update

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 // Setup empty JS object to act as endpoint for all routes
 let projectData = {};
+
 // Require Express to run server and routes
 var express = require('express')
+
 // Start up an instance of app
 const app = express()
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 // Cors for cross origin allowance
 var cors = require('cors')
 app.use(cors());
+
 // Initialize the main project folder
 app.use(express.static('website'));
+
 // GET route to return projectData
 app.get('/all', (req, res) => {
-  
   res.send(projectData);
 });
 
@@ -33,6 +38,6 @@ app.post('/add', (req, res) => {
 let port=3000;
 const server = app.listen(port, listening);
 function listening(){
-    // console.log(server);
+    console.log("Nada Obaid")
     console.log(`running on localhost: ${port}`);
 };
--- website/app.js
+++ website/app.js
@@ -1,28 +1,59 @@
-/* Global Variables */
-const apiKey = 'c2c2e03266826ccaf2b58838e228fe88&units=imperial';
+// Global Variables
+const apiKey = 'a55817b2e50d5e782661359b404013e5&units=imperial';
 const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
-// Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
 
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+    e.preventDefault(); 
+    const newZip = document.getElementById('zip').value;
+    const feelings = document.getElementById('feelings').value;
+
+    getWeatherData(newZip)
+        .then(function (data) {
+            if (data && data.main) {
+                return postData('http://localhost:3000/add', {
+                    temperature: data.main.temp,
+                    date: newDate,
+                    userResponse: feelings
+                });
+            } else {
+                throw new Error('No data returned...');
+            }
+        })
+        .then(() => retrieveData())
+        .catch((error) => {
+            console.log("Error in performAction:", error);
+            document.getElementById('temp').innerHTML = 'Error retrieving temperature';
+            document.getElementById('content').innerHTML = 'Error retrieving user response';
+            document.getElementById('date').innerHTML = 'Error retrieving date';
+        });
+    }
+
+    
 // Function to GET Web API Data
 const getWeatherData = async (zip) => {
-    const res = await fetch(`${baseURL}?zip=${zip}&appid=${apiKey}`);
+    console.log(`Fetching weather data for zip: ${zip}`);
     try {
+        const res = await fetch(`${baseURL}?zip=${zip}&appid=${apiKey}`);
         const data = await res.json();
         if (data.cod !== 200) {
             throw new Error(data.message);
         }
+        console.log("Weather data:", data);
         return data;
     } catch (error) {
-        console.log("error", error);
+        console.log("Error:", error);
     }
 };
 
 // Function to POST data
 const postData = async (url = '', data = {}) => {
     try {
-        const response = await fetch('http://localhost:3000' + url, {
+        const response = await fetch(url, {
             method: 'POST',
             credentials: 'same-origin',
             headers: {
@@ -33,55 +64,22 @@ const postData = async (url = '', data = {}) => {
         const newData = await response.json();
         return newData;
     } catch (error) {
-        console.log("error in postData", error);
+        console.log("Error:", error);
     }
 };
 
-
-// Function to GET Project Data and update UI
+// Function to GET Project Data 
 const retrieveData = async () => {
+    console.log("Retrieving project data");
     try {
         const request = await fetch('http://localhost:3000/all');
         const allData = await request.json();
-        console.log("Data retrieved from server:", allData);
-        // Write updated data to DOM elements
+        console.log("Retrieved data:", allData);
         document.getElementById('temp').innerHTML = Math.round(allData.temperature) + ' degrees';
         document.getElementById('content').innerHTML = allData.userResponse;
         document.getElementById('date').innerHTML = allData.date;
     } catch (error) {
-        console.log("error in retrieveData", error);
+        console.log("Error:", error);
     }
 };
 
-
-// Event listener to add function to existing HTML DOM element
-document.getElementById('generate').addEventListener('click', performAction);
-
-function performAction(e) {
-    const newZip = document.getElementById('zip').value;
-    const feelings = document.getElementById('feelings').value;
-
-    getWeatherData(newZip)
-        .then(function (data) {
-            if (data && data.main) {
-                postData('/add', {
-                    temperature: data.main.temp,
-                    date: newDate,
-                    userResponse: feelings
-                })
-                .then(() => {
-                    retrieveData();
-                });
-            } else {
-             document.getElementById('temp').innerHTML = '';
-                document.getElementById('content').innerHTML = '';
-                document.getElementById('date').innerHTML = '';
-            }
-        })
-        .catch((error) => {
-            console.log("Error: ", error);
-            document.getElementById('temp').innerHTML = '';
-            document.getElementById('content').innerHTML = '';
-            document.getElementById('date').innerHTML = '';
-        });
-}
